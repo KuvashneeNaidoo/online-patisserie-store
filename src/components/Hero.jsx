1880be6bff29bbdfc30a9e1b8f30cb19e7e3968f
Kuvashnee Naidoo <105747929+KuvashneeNaidoo@users.noreply.github.com>
2025-01-04 16:17:22 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./styles.css";
+import chocolateCake from "../images/chocolate-cake.jpg";
 
 function Hero() {
   return (
     <section id="about" className="hero d-flex align-items-center">
+      <img
+        src={chocolateCake}
+        alt="Hero Image"
+        loading="lazy"
+        className="hero-image"
+      />
       <div className="hero-content text-center text-white">
         <h1>Our Desserts</h1>
       </div>
